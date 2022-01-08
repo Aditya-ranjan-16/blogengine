5957de99b5543d7d091f0898520f6d0970ae1747
Rohit Kharsan <rkharsan64@gmail.com>
2022-01-08 16:08:20 +0530

    Add axios and set baseURL

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from "react-dom";
 import CssBaseline from "@mui/material/CssBaseline";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 
+// Set up axios
+import axios from "axios";
+axios.defaults.baseURL = "http://krs-blogengine-api.herokuapp.com/api";
+
 import App from "./App";
 
 // Just use the default theme
